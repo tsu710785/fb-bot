eba4cbe98432a4db706d3bda4092aadeb0bfef96
YiChun Chang <hypersonic5421@gmail.com>
2017-02-04 01:58:08 +0800

    update message function

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ function sendTextMessage(recipientId, messageText) {
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
+    qs: { access_token: PAGE_TOKEN },
     method: 'POST',
     json: messageData
 
