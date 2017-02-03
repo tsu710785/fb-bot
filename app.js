eae353da70666f31333a5bbb36bc5fe29596e59f
YiChun Chang <hypersonic5421@gmail.com>
2017-02-04 01:38:40 +0800

    update homepage

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import request from 'request';
-import config from './config';
 
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const PAGE_TOKEN = process.env.PAGE_TOKEN;
